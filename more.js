e7750c4c1d5c39c52c1e9b9b61ff2dc0f8d0dbb0
alwaysbayou <ayoubadslive@gmail.com>
2017-02-10 15:32:07 +0100

    Update more.js

--- more.js
+++ more.js
@@ -1,13 +1,10 @@
 var h = $('div')[0].scrollHeight;
-
-
 $('#more').click(function(e) {
     e.stopPropagation();
     $('div').animate({
         'height': h
     })
 });
-
 $(document).click(function() {
     $('div').animate({
         'height': '50px'
