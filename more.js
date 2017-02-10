47210a3309a54c87482928f3afca8781de3eb568
alwaysbayou <ayoubadslive@gmail.com>
2017-02-10 16:11:50 +0100

    Update more.js

--- more.js
+++ more.js
@@ -1,12 +1,33 @@
-var h = $('div')[0].scrollHeight;
-$('#more').click(function(e) {
-    e.stopPropagation();
-    $('div').animate({
-        'height': h
-    })
+$(document).ready(function() {
+	var showChar = 100;
+	var ellipsestext = "...";
+	var moretext = "more";
+	var lesstext = "less";
+	$('.more').each(function() {
+		var content = $(this).html();
+
+		if(content.length > showChar) {
+
+			var c = content.substr(0, showChar);
+			var h = content.substr(showChar-1, content.length - showChar);
+
+			var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+			$(this).html(html);
+		}
+
+	});
+
+	$(".morelink").click(function(){
+		if($(this).hasClass("less")) {
+			$(this).removeClass("less");
+			$(this).html(moretext);
+		} else {
+			$(this).addClass("less");
+			$(this).html(lesstext);
+		}
+		$(this).parent().prev().toggle();
+		$(this).prev().toggle();
+		return false;
+	});
 });
-$(document).click(function() {
-    $('div').animate({
-        'height': '50px'
-    })
-})
