24658f669b8c285238f7be5d15239845dd337708
alwaysbayou <ayoubadslive@gmail.com>
2017-02-10 15:28:32 +0100

    Create more.js

---
+++ more.js
@@ -0,0 +1,15 @@
+var h = $('div')[0].scrollHeight;
+
+
+$('#more').click(function(e) {
+    e.stopPropagation();
+    $('div').animate({
+        'height': h
+    })
+});
+
+$(document).click(function() {
+    $('div').animate({
+        'height': '50px'
+    })
+})
